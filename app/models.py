cc3e144f07741a01d748163127cfa8f94d1b16ef
mujmilk <sasag.ks@gmail.com>
2022-12-19 01:10:42 +0900

    add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=40)
+    content = models.TextField() #post_content
+    created_at = models.DateTimeField(auto_now_add=True)
+    #edited bin
+    updated_at = models.DateTimeField(auto_now=True)
+    #summary = models.CharField(max_length=250)
+    #post_user INT
+
+    def __str__(self):
+        return self.title
